fb7c3e709d5978d2accafa387ab8f693dfc1b9c2
Park Joyeong <drhamst@gmail.com>
2021-04-15 21:35:07 +0900

    Everything is ready

--- src/main/java/com/jobs/springandreact/WelcomeController.java
+++ src/main/java/com/jobs/springandreact/WelcomeController.java
@@ -1,4 +1,14 @@
 package com.jobs.springandreact;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
 public class WelcomeController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello, the time at the server is now " + new Date() + "\n";
+    }
 }
